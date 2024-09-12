913decd652d6f386da4112cc8073b63017937315
huyhoangy <huyhoangwf@gmail.com>
2024-09-12 20:42:56 +0700

    commit

---
+++ bai16_for.java
@@ -0,0 +1,13 @@
+public class bai16_for {
+    public static void main(String[] args) {
+     for (int i=2; i<12 ; i+=2){
+         System.out.println( " i= " +i);
+     }
+     //tinh tổng
+        int tong =0 ;
+        for ( int i =0 ; i<=10 ; i++ ){
+            tong += i;
+        }
+        System.out.println( " tong la "+ tong );
+    }
+}
